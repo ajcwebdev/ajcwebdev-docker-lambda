9a847c96570c279f99d545f8725875ab36fbfd9f
Anthony Campolo <anthony@quicknode.com>
2022-05-03 17:58:56 -0500

    update project

--- Dockerfile
+++ Dockerfile
@@ -1,3 +1,5 @@
-FROM public.ecr.aws/lambda/nodejs:12
+FROM public.ecr.aws/lambda/nodejs:14-arm64
+
 COPY app.js ./
+
 CMD ["app.handler"]
--- app.js
+++ app.js
@@ -1,14 +1,11 @@
 'use strict'
 
 module.exports.handler = async (event) => {
+  const message = `Cause I don't want a server, but I do still want a container`
   return {
     statusCode: 200,
     body: JSON.stringify(
-      {
-        message: `Cause I don't want a server, but I do still want a container`,
-      },
-      null,
-      2
+      { message }, null, 2
     ),
   }
 }
